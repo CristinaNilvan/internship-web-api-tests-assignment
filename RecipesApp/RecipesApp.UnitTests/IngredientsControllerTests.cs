03bfeed91fa372289b3f4dd6584d6194c1891682
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-09-01 16:08:19 +0300

    Finalize unit tests

--- RecipesApp/RecipesApp.UnitTests/IngredientsControllerTests.cs
+++ RecipesApp/RecipesApp.UnitTests/IngredientsControllerTests.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
+using RecipesApp.Application.Ingredients.Commands;
 using RecipesApp.Application.Ingredients.Queries;
 using RecipesApp.Domain.Enums;
 using RecipesApp.Domain.Models;
@@ -19,7 +20,60 @@ namespace RecipesApp.UnitTests
         private Mock<ILogger<IngredientsController>> _mockLogger = new ();
 
         [Fact]
-        public async Task Get_Ingredient_By_Id_ShouldReturnOkStatusCode()
+        public async Task Create_Ingredient_ShouldReturnNoContent()
+        {
+            // arrange
+            _mockMediator
+                .Setup(mediator => mediator.Send(It.IsAny<CreateIngredient>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Ingredient
+                {
+                    Id = 2,
+                    Name = "Ing2",
+                    Category = IngredientCategory.Dairy,
+                    Calories = 187.75f,
+                    Fats = 137.41f,
+                    Carbs = 229.27f,
+                    Proteins = 186.51f,
+                    Approved = false,
+                });
+
+            _mockMapper
+                .Setup(mapper => mapper.Map<IngredientGetDto>(It.IsAny<Ingredient>()))
+                .Returns(new IngredientGetDto
+                {
+                    Id = 2,
+                    Name = "Ing2",
+                    Category = IngredientCategory.Dairy,
+                    Calories = 187.75f,
+                    Fats = 137.41f,
+                    Carbs = 229.27f,
+                    Proteins = 186.51f,
+                    IngredientImage = null,
+                });
+
+            var ingredientPutPostDto = new IngredientPutPostDto
+            {
+                Name = "Ing2",
+                Category = IngredientCategory.Dairy,
+                Calories = 187.75f,
+                Fats = 137.41f,
+                Carbs = 229.27f,
+                Proteins = 186.51f,
+            };
+
+            var ingredientsController = new IngredientsController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object);
+
+            // act
+            var result = await ingredientsController.CreateIngredient(ingredientPutPostDto);
+
+            // assert
+            var okResult = result as CreatedAtActionResult;
+
+            Assert.Equal((int)HttpStatusCode.Created, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_Ingredient_By_Id_ShouldReturnFoundIngredient()
         {
             // arrange
             _mockMediator
@@ -37,6 +91,51 @@ namespace RecipesApp.UnitTests
                     IngredientImage = null,
                 });
 
+            var ingredientDto = new IngredientGetDto
+            {
+                Id = 2,
+                Name = "Ing2",
+                Category = IngredientCategory.Dairy,
+                Calories = 187.75f,
+                Fats = 137.41f,
+                Carbs = 229.27f,
+                Proteins = 186.51f,
+                IngredientImage = null,
+            };
+
+            _mockMapper
+                .Setup(mapper => mapper.Map<IngredientGetDto>(It.IsAny<Ingredient>()))
+                .Returns(ingredientDto);
+
+            var ingredientsController = new IngredientsController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object);
+
+            // act
+            var result = await ingredientsController.GetIngredientById(2);
+
+            // assert
+            var okResult = result.Result as OkObjectResult;
+
+            Assert.Equal(ingredientDto, okResult.Value);
+        }
+
+        [Fact]
+        public async Task Update_Ingredient_ShouldReturnNoContent()
+        {
+            // arrange
+            _mockMediator
+                .Setup(mediator => mediator.Send(It.IsAny<UpdateIngredient>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Ingredient
+                {
+                    Id = 2,
+                    Name = "Ing2",
+                    Category = IngredientCategory.Dairy,
+                    Calories = 187.75f,
+                    Fats = 137.41f,
+                    Carbs = 229.27f,
+                    Proteins = 186.51f,
+                    Approved = false,
+                });
+
             _mockMapper
                 .Setup(mapper => mapper.Map<IngredientGetDto>(It.IsAny<Ingredient>()))
                 .Returns(new IngredientGetDto
@@ -51,26 +150,71 @@ namespace RecipesApp.UnitTests
                     IngredientImage = null,
                 });
 
-/*            _mockLogger
-                .Setup(logger => logger.LogInformation(It.IsAny<string>()));*/
-                
+            var ingredientPutPostDto = new IngredientPutPostDto
+            {
+                Name = "Ing2",
+                Category = IngredientCategory.Dairy,
+                Calories = 187.75f,
+                Fats = 137.41f,
+                Carbs = 229.27f,
+                Proteins = 186.51f,
+            };
 
             var ingredientsController = new IngredientsController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object);
 
             // act
-            var result = await ingredientsController.GetIngredientById(2);
+            var result = await ingredientsController.UpdateIngredient(2, ingredientPutPostDto);
 
             // assert
-            var okResult = result.Result as OkObjectResult;
+            var okResult = result as NoContentResult;
 
-            Assert.Equal((int)HttpStatusCode.OK, okResult.StatusCode);
+            Assert.Equal((int)HttpStatusCode.NoContent, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task Delete_Ingredient_ShouldReturnNoContent()
+        {
+            // arrange
+            _mockMediator
+                .Setup(mediator => mediator.Send(It.IsAny<DeleteIngredient>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Ingredient
+                {
+                    Id = 2,
+                    Name = "Ing2",
+                    Category = IngredientCategory.Dairy,
+                    Calories = 187.75f,
+                    Fats = 137.41f,
+                    Carbs = 229.27f,
+                    Proteins = 186.51f,
+                    Approved = false,
+                    IngredientImage = null,
+                });
+
+            var ingredientDto = new IngredientGetDto
+            {
+                Id = 2,
+                Name = "Ing2",
+                Category = IngredientCategory.Dairy,
+                Calories = 187.75f,
+                Fats = 137.41f,
+                Carbs = 229.27f,
+                Proteins = 186.51f,
+                IngredientImage = null,
+            };
+
+            _mockMapper
+                .Setup(mapper => mapper.Map<IngredientGetDto>(It.IsAny<Ingredient>()))
+                .Returns(ingredientDto);
+
+            var ingredientsController = new IngredientsController(_mockMediator.Object, _mockMapper.Object, _mockLogger.Object);
+
+            // act
+            var result = await ingredientsController.DeleteIngredient(2);
+
+            // assert
+            var okResult = result as NoContentResult;
+
+            Assert.Equal((int)HttpStatusCode.NoContent, okResult.StatusCode);
         }
     }
 }
-
-/*            mockMapper
-                .Setup(x => x.Map<DestinationClass>(It.IsAny<SourceClass>()))
-                .Returns((SourceClass source) =>
-                {
-                    // abstract mapping function code here, return instance of DestinationClass
-                });*/
